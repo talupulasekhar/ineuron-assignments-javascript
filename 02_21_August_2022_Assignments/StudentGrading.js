5ca445c676a86c8b8b46b1f373153aa2524b3c03
sekhar talupula <sekher.talupula@gmail.com>
2022-09-17 18:01:35 +0530

    updated code

--- 02_21_August_2022_Assignments/StudentGrading.js
+++ 02_21_August_2022_Assignments/StudentGrading.js
@@ -5,25 +5,25 @@ let input =  prompt("Enter marks of student ::: ", "100") ;
 let x = Number(input)
 
 switch(true){
-    case x>90 && x<=100 :
+    case x>=90 && x<=100 :
         console.log("S Grade");
          break;
-    case x>80 && x<=90 :
+    case x>=80 && x<90 :
         console.log("A Grade");
         break;
-    case x>70 && x<=80 :
+    case x>=70 && x<80 :
         console.log("B Grade");
         break;       
-    case x>60 && x<=70 :
+    case x>=60 && x<70 :
         console.log("C Grade");
         break;                 
-    case x>50 && x<=60 :
+    case x>=50 && x<60 :
         console.log("D Grade");
         break;                         
-    case x>40 && x<=50 :
+    case x>=40 && x<50 :
         console.log("E Grade");
         break;   
-    case x>0 && x<=40 :
+    case x>0 && x<40 :
         console.log("Student has failed");
         break;                                        
     default :
